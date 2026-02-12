f6a204b90d93ef009fd25caab7eb361e87f178a9
Shreya Rani <shreyarani2331@gmail.com>
2026-02-12 13:45:04 +0530

    student details

---
+++ day9.py
@@ -0,0 +1,12 @@
+n = input("enter your name: ")
+
+r = int(input("enter your roll no.: "))
+
+b = input("enter your branch: ")
+
+student = (n , r, b)
+
+print("Student Details")
+print("Name   :", student[0])
+print("Roll   :", student[1])
+print("Branch :", student[2])
